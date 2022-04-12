affcfa78b243a3414ba7cedc9078600f33c7416e
Jasper Zschiegner <schjaspe@amazon.de>
2022-04-12 16:37:54 +0200

    Rename to just sageinspector.

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 
 # Metadata goes in setup.cfg. These are here for GitHub's dependency graph.
 setup(
-    name="sage-inspector",
+    name="sageinspector",
     install_requires=[
         "boto3",
         "click >= 8.0",
